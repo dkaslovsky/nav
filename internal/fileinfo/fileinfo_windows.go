362afe9fa01b25b3cb3be76f772e59d2669a3654
dkaslovsky <dkaslovsky@gmail.com>
2023-09-09 14:03:42 -0600

    windows todo for possible lib to get uid/gid info

--- internal/fileinfo/fileinfo_windows.go
+++ internal/fileinfo/fileinfo_windows.go
@@ -7,6 +7,8 @@ import (
 	"io/fs"
 )
 
+// TODO: consider using https://github.com/itchio/ox/blob/12c6ca18d236/winox/permissions_windows.go#L375
+
 var (
 	ErrNoUser  = errors.New("no user information available")
 	ErrNoGroup = errors.New("no group information available")
